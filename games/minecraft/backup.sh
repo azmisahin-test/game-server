bbb2029b5eb80a74eaa6f38c59f5563b964458c4
Azmi ŞAHİN <6445951+azmisahin@users.noreply.github.com>
2024-10-24 12:06:36 +0100

    Update backup.sh

--- games/minecraft/backup.sh
+++ games/minecraft/backup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# Yedekleme dizini
+echo "Yedekleme scripti başlatılıyor..."
 BACKUP_DIR=${BACKUP_DIR}
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+echo "Yedekleme dizini: $BACKUP_DIR"
+echo "Zaman damgası: $TIMESTAMP"
 
 # Yedekleme oluştur
 tar -czvf $BACKUP_DIR/minecraft_backup_$TIMESTAMP.tar.gz /minecraft
